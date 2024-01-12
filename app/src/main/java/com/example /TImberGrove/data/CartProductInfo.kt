7daaed2d9494d1a74c8236f6de0136f64ec59942
Tarikul Islam Shykat <77191261+Tarikul-Islam-Shykat@users.noreply.github.com>
2024-01-12 20:11:34 +0600

    Create CartProductInfo.kt

---
+++ app/src/main/java/com/example /TImberGrove/data/CartProductInfo.kt
@@ -0,0 +1,12 @@
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class CartProductInfo(
+    val productInfo: ProductInfo,
+    val quantity : Int,
+    val selectedColor : Int? = null,
+    val selectedSize : String? = null
+) : Parcelable{
+    constructor() : this(ProductInfo(), 1, null, null) // for firebase.. an empty constructor is needed.
+}
