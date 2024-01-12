aceeecf2b5ec79a5f2781d6becd65b17b73b8608
Tarikul islam shykat <shykatislam138@gmail.com>
2024-01-12 19:44:49 +0600

    Revert "Beautiful and modern furniture android Application"
    This reverts commit 502a0441600a0dd5ce2bbcede0d90c8e32172ea7.

--- app/src/main/java/com/example/timbertrove/data/CategoryInfo.kt
+++
@@ -1,9 +0,0 @@
-package com.example.timbertrove.data
-
-sealed class CategoryInfo(val categoryInfo: String) {
-    object Chair : CategoryInfo("Chairs")
-    object Cupboard : CategoryInfo("Cupboard")
-    object Table : CategoryInfo("Table")
-    object Accessory : CategoryInfo("Accessory")
-    object Furniture : CategoryInfo("Furniture") // done
-}
--- app/src/main/java/com/example/timbertrove/utils/TimberTroveApplication.kt
+++
@@ -1,9 +0,0 @@
-package com.example.timbertrove.utils
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class TimberTroveApplication : Application() {
-
-}
