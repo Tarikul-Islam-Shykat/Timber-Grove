abb618fc99f1ee83d56591770d77d5e06a9f3ab3
Tarikul Islam Shykat <77191261+Tarikul-Islam-Shykat@users.noreply.github.com>
2024-01-12 20:27:05 +0600

    Create Constants.kt

---
+++ app/src/main/java/com/example /TImberGrove/utils/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.timbertrove.utils
+
+object Constants {
+    const val  USER_COLLECTION = "users"
+    const val INTRODUCTION_SHAREDPREF = "IntroductionSP"
+    const val INTRODUCTION_KEY = "IntroductionKey"
+}
